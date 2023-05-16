5d98bea9f633e7d53cb99ac8d7bba9f12cef6617
zhangqm <qimeng_zhang@intsig.net>
2023-05-16 22:23:12 +0800

    feat: isSupportFontFamily

--- index.d.ts
+++ index.d.ts
@@ -4,4 +4,22 @@
  */
 declare function getElementSelector(node: Element): string;
 
-export { getElementSelector };
+/**
+ * @param url 需要获取参数的url字符串,默认为window.location.href
+ * @returns URLSearchParams对象
+ */
+declare function getQueryParams(url?: string): URLSearchParams;
+/**
+ * @param param 需要获取的参数名字符串
+ * @param url 需要获取参数的url字符串,默认为window.location.href
+ * @returns 参数值
+ */
+declare function getQueryParam(param: string, url?: string): string | null;
+
+/**
+ * @param family =浏览器是否支持某种字体
+ * @returns 是否支持
+ */
+declare function isSupportFontFamily(family: string): boolean;
+
+export { getElementSelector, getQueryParam, getQueryParams, isSupportFontFamily };
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export * from './css'
 export * from './core'
+export * from './support'
